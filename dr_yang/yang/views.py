26fa67ceeade70c04e8340a9bc63467a70c43624
Marc Uy <marcthemarcian@gmail.com>
2015-05-20 06:35:33 +0800

    Done

--- dr_yang/yang/views.py
+++ dr_yang/yang/views.py
@@ -5,6 +5,8 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.core.urlresolvers import reverse
 
 from yang.forms import AssessmentForm
+import pickle
+import os
 
 class IndexView(generic.TemplateView):
     template_name = "yang/index.html"
@@ -21,13 +23,45 @@ class AssessView(FormView):
     def post(self, *args, **kwargs):
         form = AssessmentForm(self.request.POST)
 
-        print self.request.POST # naa diri sud ang 13 vars
-
         if form.is_valid():
+            input_array = [];
+            input_array.append(self.request.POST['age']);
+            input_array.append(self.request.POST['sex']);
+            input_array.append(self.request.POST['chestpain']);
+            input_array.append(self.request.POST['bloodpressure']);
+            input_array.append(self.request.POST['cholesterol']);
+            input_array.append(self.request.POST['bloodsugar']);
+            input_array.append(self.request.POST['ecg']);
+            input_array.append(self.request.POST['heartrate']);
+            input_array.append(self.request.POST['angina']);
+            input_array.append(self.request.POST['stdepression']);
+            input_array.append(self.request.POST['slope']);
+            input_array.append(self.request.POST['vessels']);
+            input_array.append(self.request.POST['thal']);
+            print input_array
 
-            # do prediction here
+            module_dir = os.path.dirname(__file__)  # get current directory
+            file_path1 = os.path.join(module_dir, 'SVC_model.pkl')
+            file_path2 = os.path.join(module_dir, 'scaler.pkl')
 
-            result = 1 # place result here
+            model_pkl_file = open(file_path1, 'rb')
+            scaler_pkl_file = open(file_path2, 'rb')
+
+            model = pickle.load(model_pkl_file)
+            scaler = pickle.load(scaler_pkl_file)
+
+            scaler_pkl_file.close()
+            model_pkl_file.close()
+
+            input_array = scaler.transform(input_array)
+            
+            predict_proba = model.predict_proba(input_array)
+            print "Prediction Probabilities", predict_proba
+
+            prediction = model.predict(input_array)
+            print "Prediction", prediction
+
+            result = prediction # place result here
             self.request.session['result'] = result
             return HttpResponseRedirect(reverse('result'))
         else:
@@ -46,7 +80,7 @@ class ResultsView(generic.TemplateView):
 
 
         if context['result']:
-            context['message'] = "The patient's heart status is indicating a presence of a heart problem."            
+            context['message'] = "The patient's heart status indicates a heart problem."            
         else:
             context['message'] = "The patient's heart seems to be healthy."
         return context
