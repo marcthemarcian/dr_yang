631870ba4bf42936b750abadcf36d1fdd81a5648
Jonathan Tumulak <tumulak.jonathan@gmail.com>
2015-05-20 03:19:06 +0800

    post form and result

--- dr_yang/yang/forms.py
+++ dr_yang/yang/forms.py
@@ -11,24 +11,24 @@ slope_choice = [(1, 'Upsloping'), (2, 'Flat'), (3, 'Downsloping')]
 thal_choice = [(3, 'Normal'), (6, 'Fixed Defect'), (7, 'Reversable Defect')]
 
 
-class AssessmetForm(forms.Form):
-    age = forms.IntegerField(min_value=1, required=True)
+class AssessmentForm(forms.Form):
+    age = forms.IntegerField(min_value=1, required=True, label='How old is the patient?')
     sex = forms.ChoiceField(choices=sex_choice, widget=forms.RadioSelect(),
-                            required=True)
-    chestpain = forms.ChoiceField(choices=chestpain_choice,
-                                  widget=forms.RadioSelect(), required=True)
-    bloodpressure = forms.IntegerField(required=True)
-    cholesterol = forms.IntegerField(required=True)
-    bloodsugar = forms.ChoiceField(choices=yesno_choice,
-                                   widget=forms.RadioSelect(), required=True)
+                            required=True, label='What is the patient\'s sex?')
+    chestpain = forms.ChoiceField(choices=chestpain_choice, widget=forms.RadioSelect(),
+                                  required=True, label='What type of chest pain is the patient experiencing?')
+    bloodpressure = forms.IntegerField(required=True, label='How much is the person\'s resting blood pressure (in mmHg)?')
+    cholesterol = forms.IntegerField(required=True, label='How much is the patient\'s serum cholesterol (in mg/dl)?')
+    bloodsugar = forms.ChoiceField(choices=yesno_choice, widget=forms.RadioSelect(),
+                                   required=True, label='Does the patient have a fasting blood sugar > 120 mg/dl?')
     ecg = forms.ChoiceField(choices=ecg_choice, widget=forms.RadioSelect(),
-                            required=True)
-    heartrate = forms.IntegerField(required=True)
-    angina = forms.ChoiceField(choices=yesno_choice,
-                               widget=forms.RadioSelect(), required=True)
-    stdepression = forms.IntegerField(required=True)
-    slope = forms.ChoiceField(choices=slope_choices,
-                              widget=forms.RadioSelect(), required=True)
-    vessels = forms.IntegerField(min_value=0, max_value=3)
+                            required=True, label='What is the patient\'s resting electrocardiographic results?')
+    heartrate = forms.IntegerField(required=True, label='How much is the patient\'s maximum achieved heart rate?')
+    angina = forms.ChoiceField(choices=yesno_choice, widget=forms.RadioSelect(),
+                               required=True, label='Does that patient experience exercise-induced angina?')
+    stdepression = forms.IntegerField(required=True, label='What is the patient\'s ST depression induced by exercise?')
+    slope = forms.ChoiceField(choices=slope_choice, widget=forms.RadioSelect(),
+                              required=True, label='What is the slope of the peak exercise ST segment?')
+    vessels = forms.IntegerField(min_value=0, max_value=3, label='How many major vessels does the patient have?')
     thal = forms.ChoiceField(choices=thal_choice, widget=forms.RadioSelect(),
-                             required=True)
+                             required=True, label='Does the patient have a known heart defect?')
