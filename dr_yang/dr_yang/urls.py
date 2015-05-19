0df59d998cfef891b354707a6fa183ce39159c77
Marc Uy <marcthemarcian@gmail.com>
2015-05-20 01:31:57 +0800

    go atan

--- dr_yang/dr_yang/urls.py
+++ dr_yang/dr_yang/urls.py
@@ -3,10 +3,16 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+from yang import views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'dr_yang.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.IndexView.as_view(), name="index"),
+    url(r'^questionnaire/', views.QuestionnaireView.as_view(), name="questionnaire"),
+    url(r'^assess/', views.AssessView.as_view(), name="assess"),
+    url(r'^about/', views.AboutView.as_view(), name="about"),
 )
